332a3814eaf18e8c3b0b79ca8bac4a6a61687f7c
Aaron <aaron@cute.engineer>
2026-09-07 11:08:39 +1000

    Add dev proxy to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [process.env.VITE_ALLOWED_HOST!],
+  },
   plugins: [react()],
-})
+});
